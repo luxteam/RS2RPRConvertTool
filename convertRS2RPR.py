02170bde64759bf8e94b08a08cda893a3da5767c
Andrei Kravchenko <epozine@inbox.ru>
2020-05-19 19:47:05 +0300

    v.2.4.0 Add license

--- convertRS2RPR.py
+++ convertRS2RPR.py
@@ -1,3 +1,14 @@
+# Copyright 2020 Advanced Micro Devices, Inc
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Redshift to RadeonProRender Converter
 
 import maya.mel as mel
@@ -7,7 +18,7 @@ import os
 import math
 import traceback
 
-RS2RPR_CONVERTER_VERSION = "2.3.2"
+RS2RPR_CONVERTER_VERSION = "2.4.0"
 
 # log functions
 
